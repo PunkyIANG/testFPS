e5c00d056841d541624eefd6f5878929cb2ebbf5
zapto123 <tsoorckanukris@gmail.com>
2020-02-09 19:11:48 +0200

    more stuff, need to implement camera rotation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public float speed = 10f;
+    public float mouseSensitivityX = 10f;
+    public float mouseSensitivityY = 10f;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        var moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));  //getting the input
+        moveDirection = transform.TransformDirection(moveDirection);                                        //localspace -> worldspace
+        rigidbody.MovePosition(transform.position + moveDirection * speed * Time.fixedDeltaTime);           //applying movement
+
+        var rotX = Input.GetAxis("Mouse X") * Time.fixedDeltaTime * mouseSensitivityX;
+        var rotY = Input.GetAxis("Mouse Y") * Time.fixedDeltaTime * mouseSensitivityY;
+
+        
+    }
+}
---
+++ Assets/Scripts/SphereGizmo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereGizmo : MonoBehaviour
+{
+    public Color gizmosColor;
+    public float radius;
+    void OnDrawGizmos()
+    {
+        Gizmos.color = gizmosColor;
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+}
