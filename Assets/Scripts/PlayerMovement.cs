3871ee2535354d56732ef3e111256acceebe6a13
zapto123 <tsoorckanukris@gmail.com>
2020-02-13 20:26:47 +0200

    moar stuff

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,33 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 [RequireComponent(typeof(Rigidbody))]
 
 public class PlayerMovement : MonoBehaviour
 {
     private Mouse mouse;
+    private Keyboard keyboard;
     new Rigidbody rigidbody;
     public float speed = 10f;
-    public float mouseSensitivity = 10f;
+    public Vector2 mouseSensitivity = new Vector2(10f,20f);
     public float maxCameraXAngle = 90f;
     public bool invertY = false;
 
+    bool rotated = false;
     Camera mainCam;
     Vector3 previousMousePosition;
+    Quaternion lateRotation;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         Cursor.visible = false;
         mouse = Mouse.current;
+        keyboard = Keyboard.current;
         mainCam = GetComponentInChildren<Camera>();
-        mouse.delta.y.invert = !invertY;
+        QualitySettings.maxQueuedFrames = 0;
     }
 
     void Update() {     //we're handling input in update for better gameplay feel
-        CharMovement();
+        //CharMovement();
+        var wishDir = new Vector3 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Jump"), Input.GetAxisRaw("Vertical"));
+        PM_Accelerate(wishDir, speed, 1f);
         CameraRotation();
+        if (rotated)
+            RotateCamera(lateRotation);
+
     }
+
+    
+
+/*     void LateUpdate() {
+    }
+ */
 
     void CharMovement() {
         var moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));  //getting the input
@@ -37,25 +51,33 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void CameraRotation() {
-        var mouseDelta = GetMouseInputAsAPIDelta() * mouseSensitivity;
+        if (GetMouseInputAsAPIDelta() != Vector2.zero) {
+            var mouseDelta = Vector2.Scale(GetMouseInputAsAPIDelta(), mouseSensitivity);
 
-        rigidbody.MoveRotation(rigidbody.rotation * Quaternion.Euler(0f, mouseDelta.x, 0f));
-        
-        //clamp it
+            rigidbody.MoveRotation(rigidbody.rotation * Quaternion.Euler(0f, mouseDelta.x, 0f));
+            
+            //clamp it
 
-        var rawCameraRotation = mainCam.transform.rotation * Quaternion.Euler(mouseDelta.y, 0f, 0f);    //getting the unclamped rotation
+            var rawCameraRotation = mainCam.transform.rotation * Quaternion.Euler(mouseDelta.y, 0f, 0f);    //getting the unclamped rotation
 
-        if (Quaternion.Angle(rigidbody.rotation, rawCameraRotation) > maxCameraXAngle)  {           //if the rotation x angle is too big
-            var lookAtPosition = mainCam.transform.TransformDirection(Vector3.forward);         //we check the lookAt direction
-            if (Vector3.Dot(lookAtPosition, Vector3.up) > 0f) {                                     //if player looks up
-                mainCam.transform.localRotation = Quaternion.AngleAxis(-90f, Vector3.right);    //we clamp it up
+            if (Quaternion.Angle(rigidbody.rotation, rawCameraRotation) > maxCameraXAngle)  {           //if the rotation x angle is too big
+                var lookAtPosition = mainCam.transform.TransformDirection(Vector3.forward);         //we check the lookAt direction
+                if (Vector3.Dot(lookAtPosition, Vector3.up) > 0f) {                                     //if player looks up
+                    lateRotation = Quaternion.AngleAxis(-maxCameraXAngle, Vector3.right);    //we clamp it up
+                } else {
+                    lateRotation = Quaternion.AngleAxis(maxCameraXAngle, Vector3.right);     //else we clamp it down
+                }
             } else {
-                mainCam.transform.localRotation = Quaternion.AngleAxis(90f, Vector3.right);     //else we clamp it down
+                lateRotation = mainCam.transform.localRotation * Quaternion.Euler(mouseDelta.y, 0f, 0f);               //if not, we just add the rotation as usual
             }
+
+            rotated = true;
         } else {
-            mainCam.transform.rotation *= Quaternion.Euler(mouseDelta.y, 0f, 0f);               //if not, we just add the rotation as usual
+            rotated = false;
         }
+        
     }
+
 
     Vector2 GetMouseInputAsPositionDelta()
     {
@@ -65,6 +87,37 @@ public class PlayerMovement : MonoBehaviour
     }
 
     Vector2 GetMouseInputAsAPIDelta() {
-        return mouse.delta.ReadUnprocessedValue();
+        var result = mouse.delta.ReadUnprocessedValue();
+
+        if (result != Vector2.zero)
+            print(result);
+
+        if (!invertY)
+            result.y *= -1;
+
+        return result;
+    }
+
+    void RotateCamera(Quaternion rotation) {
+        mainCam.transform.localRotation = rotation;
+    }
+
+    void PM_Accelerate(Vector3 wishDir, float wishSpeed, float accel) {
+        int	i;
+        float addspeed, accelspeed, currentspeed;
+
+        currentspeed = Vector3.Dot(rigidbody.velocity, wishDir);
+        addspeed = wishSpeed - currentspeed;
+        if (addspeed <= 0) {
+            return;
+        }
+
+        accelspeed = accel * Time.deltaTime * wishSpeed;
+        if (accelspeed > addspeed) {
+            accelspeed = addspeed;
+        }
+
+        wishDir = transform.TransformDirection(wishDir);
+        rigidbody.MovePosition(transform.position + accelspeed * wishDir);        
     }
 }
---
+++ Assets/Scripts/framerate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class framerate : MonoBehaviour
+{
+    Text fpsMeter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fpsMeter = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fpsMeter.text = (int) (1 / Time.deltaTime) + " FPS";
+    }
+}
