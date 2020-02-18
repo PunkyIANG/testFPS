6254fdb95ada01a807dec28b533901fc0352d9e4
zapto123 <tsoorckanukris@gmail.com>
2020-02-18 21:42:55 +0200

    more changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -31,7 +31,7 @@ public class PlayerMovement : MonoBehaviour
     private bool wishJump;
     private Vector3 abstractInput;
     private Vector3 playerVelocity = Vector3.zero;
-
+    private Vector3 startPosition;
 
     bool rotated = false;
     Camera mainCam;
@@ -45,6 +45,7 @@ public class PlayerMovement : MonoBehaviour
         keyboard = Keyboard.current;
         mainCam = GetComponentInChildren<Camera>();
         QualitySettings.maxQueuedFrames = 0;
+        startPosition = transform.position;
     }
 
     // void Update() {     //we're handling input in update for better gameplay feel
@@ -72,6 +73,8 @@ public class PlayerMovement : MonoBehaviour
         else
             AirMove();
 
+        if (playerVelocity.y != 0f)
+            print(playerVelocity.y);
         rigidbody.MovePosition(rigidbody.position + playerVelocity * Time.deltaTime);
 
     }
@@ -170,8 +173,8 @@ public class PlayerMovement : MonoBehaviour
         Accelerate(wishDir, wishSpeed, runAcceleration);
 
         // Reset the gravity velocity
-        if (useGravity)
-            playerVelocity.y = -gravity * Time.deltaTime;
+        //if (useGravity)
+        //    playerVelocity.y = -gravity * Time.deltaTime;
 
         if(wishJump)
         {
@@ -274,9 +277,10 @@ public class PlayerMovement : MonoBehaviour
             return;
         }
 
-        if(Input.GetButtonDown("Jump") && !wishJump)
+        if (Input.GetButtonDown("Jump") && !wishJump)
             wishJump = true;
-        if(Input.GetButtonUp("Jump"))
+        
+        if (Input.GetButtonUp("Jump"))
             wishJump = false;
     }
 
@@ -311,6 +315,12 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 TestMovement() {
         return new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+    }
+
+    public void ResetPlayer (InputAction.CallbackContext context)
+    {
+        transform.position = startPosition;
+        print("reset");
     }
 
 }
